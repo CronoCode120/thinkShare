eccce851b90f257bf2da566273f755c6b4a63a41
CronoCode120 <ctaverafernandez@gmail.com>
2023-06-19 11:41:36 +0200

    removed pointer-events fix

--- components/PostComponent.jsx
+++ components/PostComponent.jsx
@@ -59,7 +59,6 @@ const Post = ({users, post, nowDate}) => {
           <div className='flex justify-end pt-3'>
             <div id='likeBtn' title='Me gusta' className='w-fit h-8 px-2 bg-gray-800 hover:bg-red-400 hover:shadow-lg hover:shadow-red-500 rounded-xl hover:text-black duration-200 flex justify-evenly items-center select-none cursor-pointer mr-2 sm:mr-4'
               onClick={async (e) => {
-                e.target.style.pointerEvents = 'none';
                 if(likedByUser) {
                   setLikedByUser(false);
                   if (currentlyLiked) {
@@ -77,7 +76,6 @@ const Post = ({users, post, nowDate}) => {
                   }
                   await likePost(post._id, sessionUser._id, 'like');
                 }
-                e.target.style.pointerEvents = 'auto';
               }}>
               <span><FontAwesomeIcon icon={likedByUser ? faSolidHeart : faHeart} size='xl'/></span> <span className='font-semibold pl-2'>{likesNumber}</span>
             </div>
